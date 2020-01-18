b9a7c3fd07b8bc19696a4cfeed0b42aaecb89b21
KayLee <caleboau2012@gmail.com>
2020-01-19 00:28:27 +0100

    Setting for Heroku

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 LOGIN_REDIRECT_URL = "player_home"
 LOGOUT_REDIRECT_URL = "tictactoe_welcome"
 LOGIN_URL = "player_login"
+
+django_heroku.settings(locals())
